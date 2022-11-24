f0ef90c46ee0081972e28ebce530e7282ed35e62
Yaron Peres <yaronp16@gmail.com>
2022-11-24 17:48:13 +0100

    search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+const Search = (props) => {
+    const searchChangeHandler = (event) => {
+        props.onSearch(event.target.value)
+    }
+    return (
+        <div>
+            <input type="text" onChange={searchChangeHandler}></input>
+        </div>
+    )
+}
+export default Search
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Search from "../components/Search.jsx";
 
+const apiEndpoint = "http://localhost:8000/api/places";
+console.log(apiEndpoint);
 export default function Home() {
-   return (
-      <div>Home</div>
-   )
+  const [places, setPlaces] = useState([]);
+  const [filterPlaces, setFilterPlaces] = useState([]);
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const res = await axios.get(apiEndpoint);
+      setPlaces(res.data);
+      setFilterPlaces(res.data);
+      console.log(res.data);
+    };
+    apiCall();
+  }, []);
+  const searchHandler = (search) => {
+    const searchThis = places.filter((one) =>
+      one.name.toLowerCase().toUpperCase().includes(search)
+    );
+    setFilterPlaces(searchThis);
+  };
+
+  return (
+   <div>
+      <h1>list</h1>
+      <Search onSearch={searchHandler} />
+      <ul>
+        {filterPlaces.map((place) => {
+            return (
+               <li key={place._id}>
+               <h2>{place.name}</h2>
+               <img src={place.pictures} alt="" />
+               <p>{place.address}</p>
+               <p>{place.Review}</p>
+               <Link to={`/places/${place._id}`}>More Details</Link>
+               </li>
+            );
+         })}
+      </ul>
+   </div>
+  );
 }
