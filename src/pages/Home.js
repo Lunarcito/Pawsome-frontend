65b41fbee7874f1b90db9cae0301da6d59f25f92
Giuliano Sacco <saccogiuliano99@gmail.com>
2022-11-28 17:07:29 +0100

    tt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,6 +50,7 @@ export default function Home() {
   return (
     <div>
       <h1>list</h1>
+      <h2>hello</h2>
       <Search onSearch={searchHandler} />
       <Filter setActiveType={setActiveType}/>
       <ul>
