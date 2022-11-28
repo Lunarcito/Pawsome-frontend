472a17a141fae877530d8421786013d5765d4b0a
Yaron Peres <yaronp16@gmail.com>
2022-11-28 17:04:27 +0100

    pet profile and profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,6 @@ export default function Navbar() {
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink></li>}
             {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/">Login</NavLink></li>}
             {isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/profile">User Profile</NavLink></li>}
-            {isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/Favorites">Favorites</NavLink></li>}
             {isLoggedIn && <li><button onClick={() => logOutUser()}>Log out</button></li>}
          </ul>
       </div>
--- src/pages/PetProfile.js
+++ src/pages/PetProfile.js
@@ -1,22 +1,40 @@
-import React from 'react'
-import './UserProfile.css';
+
 import {Link} from "react-router-dom";
 import myPetImg from "../assets/mypet.png"
-
-
 import './UserProfile.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
+const storedToken = localStorage.getItem("authToken");
+const apiEndpoint = "http://localhost:8000/api/pet-profile"
 
 const PetProfile =() => {
+  const [ pets, setPets] = useState([])
+  useEffect (() => {
+    const apiCall = async () => {
+      const res = await axios.get(apiEndpoint, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+    })
+      setPets(res.data)
+      console.log(res.data)
+    }
+    apiCall()
+  }, [])
 
     return (
       <div className="petprofile">
         <ul>
-          <Link to="/pet-profile/create">
-            <img src={myPetImg} />
-            My Pet
-          </Link>
-          <br></br>
+          {pets.map((e) => {
+            return(
+              <div key={e._id}>
+                <li>Pet Name: {e.namePet}</li>
+                <img className="petProfile" src={e.image} alt=""/>
+              </div>
+            )
+          })}
+
         </ul>
+          <Link to="/pet-profile/create"><img src={myPetImg} />Add a pet</Link>
       </div>
     );
     
