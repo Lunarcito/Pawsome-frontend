9c00af66227883c0555c4e5d38c1ac19820e2958
Carolina Calle <calle.carolina@gmail.com>
2022-11-28 14:38:08 +0100

    Detailsupdated

--- src/pages/places/PlaceDetails.js
+++ src/pages/places/PlaceDetails.js
@@ -2,21 +2,23 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Map from "../../components/Map";
+import { useNavigate } from "react-router-dom";import Map from "../../components/Map";
 
 
 const apiEndpoint = "http://localhost:8000/api/places/"
+const apiEndpoint2 ="http://localhost:8000/api/favorite/"
 
 function PlaceDetails() {
     const { placeId } = useParams()
     const [place, setPlace] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         const apiCall = async () => {
             try{
-
-            const res = await axios.get((apiEndpoint + placeId))
-            setPlace(res.data)
+                const storedToken = localStorage.getItem("authToken");
+                const res = await axios.get((apiEndpoint + placeId))
+                setPlace(res.data)
             }catch(error){
                 console.log(error)
             } 
@@ -24,13 +26,34 @@ function PlaceDetails() {
         apiCall()
     }, [placeId])
 
-console.log(place)
+    const addFavoriteHandler = async (event) => { 
+      
+        try{
+            const storedToken = localStorage.getItem("authToken");
+            const res = await axios.post(
+                apiEndpoint2 + placeId,
+                { headers: { Authorization: `Bearer ${storedToken}` }})
+                
+                navigate("/favorites")
+
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+
+
     return (
         <div>
             {place && <div>
-                <h1>{place.name}</h1>
-                <p>{place.address}</p>
+                <h1>Name:{place.name}</h1>
+                <p>Address:{place.address}</p>
+                <p>Description:{place.description}</p>
+                <p>Picture:{place.pictures}</p>
+                <p>Type:{place.type}</p>
+                <p>SocialMedia:{place.socialMedia}</p>
                 <Link to={`/user-profile/${place.User._id}`}>UserProfile</Link>
+                <button onClick={addFavoriteHandler}>Add to Favorites</button>   
                 <Map/>
             </div>}
         </div>
