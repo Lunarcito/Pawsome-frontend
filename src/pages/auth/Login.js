c1888067673a604d924a8ee2526ebfffb32e9ceb
Yaron Peres <yaronp16@gmail.com>
2022-11-29 12:39:30 +0100

    error msg

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -34,7 +34,7 @@ export default function Login() {
          navigate('home');
 
       } catch (error) {
-         setErrorMessage(error.response.data.error)
+         setErrorMessage(error.response.data.message)
       }
    }
 
