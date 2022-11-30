a1ae5e431b9b09ce89e2229fab70543694f0fb1b
Yaron Peres <yaronp16@gmail.com>
2022-11-30 18:41:39 +0100

    sticky

--- src/components/homeComponents/Navbar.js
+++ src/components/homeComponents/Navbar.js
@@ -47,14 +47,14 @@ export default function Navbar() {
             <div onClick={() =>{setOpen(!open)}}>
             <FontAwesomeIcon icon={isLoggedIn? faUser : farUser} className='menu-trigger' />
             </div>
-            <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
+            {open &&<div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
                <ul>
                   {isLoggedIn && <DropdownItem> <NavLink to="/profile"> <FontAwesomeIcon icon={ faCircleUser } />User Profile</NavLink></DropdownItem>}
                   {!isLoggedIn && <DropdownItem> <NavLink to="/signup"><FontAwesomeIcon icon={ faUserPlus } />Sign up</NavLink></DropdownItem>}
                   {!isLoggedIn && <DropdownItem> <NavLink to="/"><FontAwesomeIcon icon={ faRightToBracket } />Login</NavLink></DropdownItem>}
                   {isLoggedIn && <DropdownItem> <NavLink onClick={() => logOutUser()}><FontAwesomeIcon icon={ faRightFromBracket } />Log out</NavLink></DropdownItem>}
                </ul>
-            </div>
+            </div>}
          </div>
          <div>
             <img className='logoFooter' src="https://res.cloudinary.com/dfajfbnkr/image/upload/v1669817485/Pawsome/Logo_Paw_kinagr.png" alt='logo' /> 
