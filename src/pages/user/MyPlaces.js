59cc8763cb4d09e6da541aee0aa91d6fcd12fa94
Giuliano Sacco <saccogiuliano99@gmail.com>
2022-12-01 13:47:57 +0100

    vf

--- src/pages/user/MyPlaces.js
+++ src/pages/user/MyPlaces.js
@@ -6,6 +6,14 @@ import { useParams } from "react-router-dom";
 const apiEndpoint = "http://localhost:8000/api/profile"
 const storedToken = localStorage.getItem('authToken')
 
+const image1 = 'https://res.cloudinary.com/dfajfbnkr/image/upload/v1669895036/Pawsome/pexels-samson-katt-5255258_fxn2bg.jpg'
+const image2 = 'https://res.cloudinary.com/dfajfbnkr/image/upload/v1669895006/Pawsome/pexels-cesar-mendez-5230141_wpacpr.jpg'
+const image3 = 'https://res.cloudinary.com/dfajfbnkr/image/upload/v1669895006/Pawsome/pexels-andreas-schmolmueller-3376610_lpqob7.jpg'
+const image4 = 'https://res.cloudinary.com/dfajfbnkr/image/upload/v1669895006/Pawsome/pexels-matthias-zomer-422220_f8faal.jpg'
+const image5 = 'https://res.cloudinary.com/dfajfbnkr/image/upload/v1669895005/Pawsome/pexels-engin-akyurt-1438798_sr9ard.jpg'
+
+const images = [image1, image2, image3, image4, image5]
+
 function MyPlaces() {
     const [places, setPlaces] = useState([])
     const { placeId } = useParams()
@@ -39,7 +47,7 @@ function MyPlaces() {
                 return (
                     <div key={place._id}><div className="pplaces">
                         <h2 className="Placename">{place.name}</h2>
-                        <img className="Placeimage" src={place.pictures} alt="place" />
+                        <img className="Placeimage" src={images[Math.floor(Math.random()*4)]} alt="place" />
                         
                         </div>
                         <Link to={`edit-place/${place._id}`}>Edit</Link>
